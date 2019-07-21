691840bad5e7a4e79739bf77a8b1d2b4592301d1
Chakraborty, R. A <r.a.chakraborty@accenture.com>
2019-07-21 09:49:33 +0530

    uploading Spring Rest service

---
+++ src/main/java/com/example/spring/demo/DemoProjApplication.java
@@ -0,0 +1,24 @@
+package com.example.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class DemoProjApplication {
+	
+	@RequestMapping("/hello")
+	public String hello(){
+		return "Hello Spring 2019";
+	}
+
+	//Main method to initiate the prog
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjApplication.class, args);
+	}
+	
+
+}
